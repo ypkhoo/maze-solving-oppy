fbdfacc48f7a4f1d649dd31d023cf2088cf640c9
Yik Ping Khoo <43834752+ypkhoo@users.noreply.github.com>
2019-02-25 22:32:36 -0600

    Add files via upload
    initial code base

---
+++ robotics/movement.py
@@ -0,0 +1,64 @@
+import RPi.GPIO as GPIO 
+import time 
+from sensor import distance 
+
+def init(): 
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+
+def forward(timeFrame): 
+    init()
+    GPIO.output(7, False)
+    GPIO.output(11, True)
+    GPIO.output(13, True)
+    GPIO.output(15, False)
+    time.sleep(timeFrame)
+    GPIO.cleanup()
+
+def reverse(timeFrame): 
+    init() 
+    GPIO.output(7, True)
+    GPIO.output(11, False)
+    GPIO.output(13, False)
+    GPIO.output(15, True)
+    time.sleep(timeFrame)
+    GPIO.cleanup()
+
+def turn_left(timeFrame): 
+    init() 
+    GPIO.output(7, False)
+    GPIO.output(11, True)
+    GPIO.output(13, False)
+    GPIO.output(15, False)
+    time.sleep(timeFrame)
+    GPIO.cleanup() 
+
+def turn_right(timeFrame): 
+    init() 
+    GPIO.output(7, False)
+    GPIO.output(11, False)
+    GPIO.output(13, True)
+    GPIO.output(15, False)
+    time.sleep(timeFrame)
+    GPIO.cleanup() 
+
+def pivot_left(timeFrame): 
+    init() 
+    GPIO.output(7, False)
+    GPIO.output(11, True)
+    GPIO.output(13, False)
+    GPIO.output(15, True)
+    time.sleep(timeFrame)
+    GPIO.cleanup() 
+
+def pivot_right(timeFrame): 
+    init() 
+    GPIO.output(7, True)
+    GPIO.output(11, False)
+    GPIO.output(13, True)
+    GPIO.output(15, False)
+    time.sleep(timeFrame)
+    GPIO.cleanup() 
---
+++ robotics/remCon.py
@@ -0,0 +1,29 @@
+try: 
+    import Tkinter as tk
+except ImportError: 
+    import tkinter as tk
+
+def key_input(event): 
+    init() 
+    print("Key: ", event.char)
+    key_press = event.char 
+    sleep_time = 0.030 
+
+    if key_press.lower() == 'w': 
+        forward(sleep_time)
+    elif key_press.lower() == 's': 
+        reverse(sleep_time)
+    elif key_press.lower() == 'a': 
+        turn_left(sleep_time)
+    elif key_press.lower() == 'd': 
+        turn_right(sleep_time)
+    elif key_press.lower() == 'q': 
+        pivot_left(sleep_time)
+    elif key_press.lower() == 'e': 
+        pivot_right(sleep_time)
+    else: 
+        GPIO.cleanup()
+
+command = tk.Tk() 
+command.bind('<KeyPress>', key_input)
+command.mainloop() 
